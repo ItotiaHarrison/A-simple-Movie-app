c7bb92ff9cd12faaa4f58526f156d7ac780d2adc
Harrison Itotia <harrisonitotia@gmail.com>
2023-04-04 21:00:51 +0300

    create and import components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import './App.css';
+import Header from './components/Header';
+import Movie from './components/Movie';
+import Search from './components/Search';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
+      <Movie/>
+      <Search/>
       
     </div>
   );
