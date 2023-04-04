22441c288840e2512d0f4e00d6f8450ce5dc47fa
Harrison Itotia <harrisonitotia@gmail.com>
2023-04-04 22:01:43 +0300

    create components, import and create routes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
-import Movie from './components/Movie';
-import Search from './components/Search';
+import Home from './pages/Home';
+import Movies from './pages/Movies';
+import Search from './pages/Search';
+import Series from './pages/Series';
 
 function App() {
   return (
-    <div className="App">
-      <Header/>
-      <Movie/>
-      <Search/>
-      
-    </div>
+    <>
+      <Router>
+        <Header/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/movies" element={<Movies/>}/>
+          <Route path="/series" element={<Series/>}/>
+          <Route path="/search" element={<Search/>}/>
+          
+        </Routes>
+      </Router>
+    </>
   );
 }
 
