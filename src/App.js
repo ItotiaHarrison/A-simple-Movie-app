53decaa71a79e992caf5c4c849990c3df78c7194
Harrison Itotia <harrisonitotia@gmail.com>
2023-04-05 23:35:39 +0300

    import and style

--- src/App.js
+++ src/App.js
@@ -1,12 +1,54 @@
+import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import './App.css';
 import Header from './components/Header';
 import Home from './pages/Home';
 import Movies from './pages/Movies';
-import Search from './pages/Search';
 import Series from './pages/Series';
+import './App.css';
+
+const MOVIE_API_URL = 'https://moviesminidatabase.p.rapidapi.com/movie/imdb_id/byTitle/%7Bname%7D/'
+const SERIES_API_URL = 'https://moviesminidatabase.p.rapidapi.com/series/idbyTitle/%7Bname%7D/'
 
 function App() {
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+  
+
+  useEffect(() =>{
+    fetch(MOVIE_API_URL, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'c04ec04ea3mshebd8f228e2a5d68p1a45f2jsndadd7b53bf36',
+        'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+      }
+    })
+      .then(response => response.json())
+      .then((movies) => {
+        setMovies(movies);
+      })
+      .catch(err => console.error(err));
+  }, []);
+
+
+  useEffect(() =>{
+    fetch(SERIES_API_URL, {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': 'c04ec04ea3mshebd8f228e2a5d68p1a45f2jsndadd7b53bf36',
+        'X-RapidAPI-Host': 'moviesminidatabase.p.rapidapi.com'
+      }
+    })
+	    .then(response => response.json())
+	    .then((series) => {
+        setSeries(series);
+      })
+	    .catch(err => console.error(err));
+
+  })
+
+
+
+
   return (
     <>
       <Router>
@@ -15,7 +57,6 @@ function App() {
           <Route path="/" element={<Home/>}/>
           <Route path="/movies" element={<Movies/>}/>
           <Route path="/series" element={<Series/>}/>
-          <Route path="/search" element={<Search/>}/>
           
         </Routes>
       </Router>
